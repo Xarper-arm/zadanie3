e84334cef0f197d74e3730a7d5e422a66fd2fb52
user <user@WIN-QCRQOEHEQMD>
2023-01-24 20:08:35 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿public struct Point3D
+{
+    public double X;
+    public double Y;
+    public double Z;
+
+    public Point3D(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Point3D operator +(Point3D p1, Point3D p2)
+    {
+        return new Point3D(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+    }
+    public static Point3D operator -(Point3D p1, Point3D p2)
+    {
+        return new Point3D(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+    }
+    public static Point3D operator *(Point3D p1, Point3D p2)
+    {
+        return new Point3D(p1.X * p2.X, p1.Y * p2.Y, p1.Z * p2.Z);
+    }
+    public static Point3D operator /(Point3D p1, Point3D p2)
+    {
+        return new Point3D(p1.X / p2.X, p1.Y / p2.Y, p1.Z / p2.Z);
+    }
+
+    static void Main()
+    {
+        Point3D Point1 = new Point3D(10, 20, 25);
+        Point3D Point2 = new Point3D(15, 8, 18);
+        Point3D Pointplus = Point1 + Point2;
+        Point3D Pointminus = Point1 - Point2;
+        Point3D Pointmultiply = Point1* Point2;
+        Point3D Pointdivide = Point1 / Point2;
+
+       
+    }
+}
---
+++ zadanie3/Program.cs
@@ -0,0 +1,61 @@
+﻿struct person
+{
+    public string Name;
+    public string Surname;
+    public int Age;
+    public string Sex;
+
+    public person(string n, string s, int a, string sex)
+    {
+        Name = n;
+        Surname = s;
+        Age = a;
+        Sex = sex;
+
+
+    }
+
+    public void Displayinfo()
+    {
+        Console.WriteLine($"Name = {Name} surname = {Surname} age = {Age} sex = {Sex}");
+            }
+
+    }
+ class Program
+{
+    static void Main(string[] args)
+    {
+        person[] persons = new person[2];
+        persons[0] = new person("Hayk", "Gevorgyan", 30, "male");
+
+        persons[1].Name = "Liza";
+        persons[1].Surname = "Grigoryan";
+        persons[1].Age = 20;
+        persons[1].Sex = "female";
+
+        foreach (person person in persons)
+        {
+            person.Displayinfo();
+        }
+
+        person David = new person("David", "Khachatryan", 28, "male");
+        //David.Name = "David";
+        //David.surname = "Khachatryan";
+        //David.age = 28;
+        //David.sex = "male";
+
+        for ( int i = 0;i< persons.Length;i++)
+        {
+            int midage = persons[0].Age + persons[1].Age;
+            //Console.WriteLine(midage / persons.Length);
+        }
+        
+
+        David.Displayinfo();
+
+        
+        Console.ReadKey();
+    
+
+    }
+}
